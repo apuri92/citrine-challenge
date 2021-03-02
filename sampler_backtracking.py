77852d7b3f9d636839a9cac0b8b45b62ce80bca7
Akshat Puri <akshat.puri92@gmail.com>
2021-03-02 16:20:09 -0600

    cleanup of backtracker

--- sampler_backtracking.py
+++ sampler_backtracking.py
@@ -1,91 +1,168 @@
 #!/usr/bin/env python3
 from constraints import Constraint
 from collections import deque
+from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
+from math import log10
 import sys
 
-def unitCubeCheck(x):
+# Utility to check if point is within the unit hypercube
+def UnitCubeCheck(x):
     for _ in x:
         if _ > 1 or _ < 0:
             return False
     return True
 
-inputFile = sys.argv[1]
-outputFile = sys.argv[2]
-nPts = int(sys.argv[3])
-print(f'inputFile:  {inputFile}\noutputFile:    {outputFile}\npointsRequested:  {nPts}')
-
-cnstrnts = Constraint(inputFile)
-dim = cnstrnts.get_ndim()
-start = cnstrnts.get_example()
-step = 1
-
-exploredPoints = set()
-qPoints = deque()
-itr = 0
-while len(exploredPoints) < nPts:
-    qPoints.append(start)
-    print(f'using stepsize {step}')
+# Utility to plot points in 3D
+def PlotPoints3D(pointsExplored, dim0=0, dim1=1, dim2=2):
+    fig = plt.figure()
+    ax = plt.axes(projection="3d")
+    xs=[]
+    ys=[]
+    zs=[]
+    for _ in pointsExplored:
+        xs.append(_[dim0])
+        ys.append(_[dim1])
+        zs.append(_[dim2])
     
-    # safety condition if points are not found for whatever reason
-    if step <= 0.000000001:
-        break
+    ax.scatter3D(xs, ys, zs, c=zs, cmap='hsv')
+    plt.xlim([0,1])
+    plt.ylim([0,1])
+    plt.show()
 
-    while qPoints and len(exploredPoints) < nPts:
-        # print(qPoints)
-        # print(len(exploredPoints))
-        currPoint = qPoints.pop()
-        # print(f'curr point = {currPoint}')
-        # current point is added to the queue, always start from there
-        if tuple(currPoint) in exploredPoints and tuple(currPoint) != tuple(start):
-            continue
-        
-        for iDim in range(dim):
-            p_neg = list(currPoint)
-            p_pos = list(currPoint)
+# Utility to plot points in 2D
+def PlotPoints(pointsExplored, dim0=0, dim1=1):
+    xs=[]
+    ys=[]
+    for _ in pointsExplored:
+        xs.append(_[dim0])
+        ys.append(_[dim1])
+
+    plt.scatter(xs, ys)
+    plt.xlim([0,1])
+    plt.ylim([0,1])
+    plt.show()
+
+
+# Utility to check if vectors in answerFile satisfy constraints in constraints file
+def CheckOutputFile(constraints, answerFile):
+    print(f'\nchecking answer in {answerFile}')
+    cnstrnts = Constraint(constraints)
+    dim = cnstrnts.get_ndim()
+    with open(outputFileName) as f:
+        for itr, line in enumerate(f):
+            x=[float(_) for _ in line.strip().split(" ")]
+            # print location in checker
+            if log10(itr+1) % 1 == 0:
+                print("".join(['.']*2*int(log10(itr+1)))+str(itr+1))
             
-            p_pos[iDim]=p_pos[iDim]+step
-            p_neg[iDim]=p_neg[iDim]-step
+            # if any vector fails, return false immediately
+            if not cnstrnts.apply(x):
+                print(f'failed on {x}')
+                return False
+    
+    # return true if all points succeed
+    return True
 
-            # todo: catch 1/0 check in apply method
-            # print(f'pos{p_pos}  neg:{p_neg}')
-            try:
-                if tuple(p_pos) not in exploredPoints and cnstrnts.apply(p_pos) and unitCubeCheck(p_pos):
-                    # print(f'adding pos')
-                    qPoints.append(p_pos)
-                if tuple(p_neg) not in exploredPoints and cnstrnts.apply(p_neg) and unitCubeCheck(p_neg):
-                    # print(f'adding neg')
-                    qPoints.append(p_neg)
-            except ZeroDivisionError:
+# GenerateValidPoints uses a form of breadth first search to generate n points satisfying the constraints defined in inputFileName and saves them to outputFileName. 
+def GenerateValidPoints(inputFileName='example.txt', outputFileName='exampleOut.txt', n=50):
+    # Get the constraints and dimensionality of the probelm from the input file.
+    # The startingPoint is cast as a tuple because hashing will be required to put the found points into a set.
+    cnstrnts = Constraint(inputFileName)
+    dim = cnstrnts.get_ndim()
+    startingPoint = tuple(cnstrnts.get_example())
+    
+    # This will the step size with which the exploration will be done.
+    stepSize = 1.0/5
+
+    # Setup the set to mark points that are explored and a queue to hold the points that need to be explored
+    pointsExplored = set()
+    pointsQueue = deque()
+    
+    itr = 0
+    # Drawback is that if region is not continuous, point may not be found
+    
+    # Run while number of points found is less than the number of points requested
+    while len(pointsExplored) < nPts:
+    
+        # Print the step size being used and number of points already found
+        print(f'using stepSize {stepSize}, valid points found: {len(pointsExplored)}')
+        
+        # Search will begin from the given sample point so add it to the queue.
+        pointsQueue.append(startingPoint)
+
+        # Break if the step size becomes too small.
+        # This will typically happen when points are not being found or the number of points requested is too large
+        if stepSize <= 0.00000000001:
+            print(f'Step size is too small, exiting with {len(pointsExplored)} points found.')
+            return
+
+        # Run while the queue exists and more points are required.
+        while pointsQueue and len(pointsExplored) < nPts:
+            
+            # Get the current point from the queue.
+            # A tuple is used so it can be hashed and put into a set.
+            currPoint = pointsQueue.popleft()
+
+            # The search always starts from the given sample point.
+            # The currPoint != startingPoint condition in the while loop ensures that the search begins.
+            # Otherwise it would be skipped since the starting point may already be in the pointsExplored set.
+            if currPoint != startingPoint and currPoint in pointsExplored:
                 continue
-                
-        exploredPoints.add(tuple(currPoint))
+            
+            # Explore in positive and negative direction for each dimension in the current point.
+            for iDim in range(dim):
+                for direction in [-1, +1]:
+                    # Cast as list because mutability is required to upudate the coordinates.
+                    nextPoint = list(currPoint)
+                    nextPoint[iDim]+=(direction*stepSize)
+                    
+                    # Cast as tuple for hashability.
+                    nextPoint = tuple(nextPoint)
+                    
+                    # Add to queue if it meets requirements.
+                    # This is done in a try/except since there may be a divide by zero error when checking constraints on a point.
+                    # If there is a divide by zero, the point is not added to the queue and we can continue.
+                    try:
+                        if nextPoint not in pointsExplored and cnstrnts.apply(nextPoint) and UnitCubeCheck(nextPoint):
+                            pointsQueue.append(nextPoint)
+                    except:
+                        pass
+            
+            # Once all valid points associated to the current point are added to the queue, the current point is marked as explored.
+            pointsExplored.add(currPoint)
+            
+            # End of inner while loop.
 
-            # if len(exploredPoints) == nPts:
-            #     break
+        # Halve stepSize after all points with current stepSize within constraints are found and being search again.
+        stepSize /= 5
 
-    step /= 2
-    itr+=1
-
-# Setup output file
-out = open(outputFile, "w")
-
-print(f'len explorred={len(exploredPoints)}')
-for _ in exploredPoints:
-    out.write(" ".join(map(str, _))+'\n')
+        # End of outer while loop.
 
 
-print(f'Done! Closing {outputFile}\n')
-out.close()
+    # Setup output file and write coordinates to it. File is closed automatically
+    with open(outputFileName, "w") as out:
+        for _ in pointsExplored:
+            out.write(" ".join(map(str, _))+'\n')
+
+    print(f'Found {len(pointsExplored)} valid points for constraints in {inputFileName}. These are saved to {outputFileName}')
+    PlotPoints(pointsExplored)
+    
+    return 0
 
 
-# xs=[]
-# ys=[]
-# for _ in exploredPoints:
-#     xs.append(_[0])
-#     ys.append(_[1])
+if __name__ == '__main__':
+    inputFileName = sys.argv[1]
+    outputFileName = sys.argv[2]
+    nPts = int(sys.argv[3])
+    print(f'inputFileName:  {inputFileName}\noutputFileName:    {outputFileName}\npointsRequested:  {nPts}')
+    GenerateValidPoints(inputFileName, outputFileName, nPts)
+    
+    # Checking the answers in the output file (commented out).
+    # if CheckOutputFile(inputFileName, outputFileName):
+    #     print('all tests passed')
+    # else:
+    #     print('check results')    
+    
 
-# plt.scatter(xs, ys)
-# plt.xlim([0,1])
-# plt.ylim([0,1])
-# plt.show()
+
